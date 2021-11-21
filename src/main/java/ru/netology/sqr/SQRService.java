d360e35e51ca503dab09f6b3d7b96a7276c06081
Nadir Emirbekov <Emirbekov93@mail.ru>
2021-11-21 20:35:08 +0300

    1 commit

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,11 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int calculate(int limMin;int limMax) {
+        for (int i = 10; i <= 99; i++) {
+            if (i * i >= limMin && i * i <= limMax){
+                int counter = 0;
+        }
+    }
+}
+}
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,12 @@
+package ru.netology.sqr;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQRServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void calculate() {
+        SQRService sqrService = new SQRService();
+
+    }
+}
